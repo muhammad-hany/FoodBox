5d19f6c8b414bee5e574cb86668405d2e19511ac
Muhammad <mohammedeletreby@gmail.com>
2021-06-06 22:38:27 +0200

    finished cart fragment

--- app/src/main/java/com/ertreby/foodbox/MainActivity.kt
+++ app/src/main/java/com/ertreby/foodbox/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.cart_item_list)
+        setContentView(R.layout.activity_main)
 
     }
 
